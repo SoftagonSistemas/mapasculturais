5714f50aa2d0031489e8b067c0c725d8e2eaa91d
hermesalvesbr <hermes.alves@araripina.com.br>
2024-08-09 17:24:07 -0300

    turistico

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'app.siteName' => 'Mapa Cultural - Triunfo',
-    'app.siteDescription' => 'O Mapa Cultural é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
+    'app.siteName' => 'Mapa Cultural e Turístico - Triunfo',
+    'app.siteDescription' => 'O Mapa Cultural e Turístico é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
 
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
     'themes.active' => 'MapasCulturais\Themes\BaseV2',
