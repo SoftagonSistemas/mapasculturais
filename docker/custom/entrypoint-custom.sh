532655ce8615859da79c8465915260d32ffa054e
hermesalvesbr <hermes.alves@araripina.com.br>
2024-10-11 10:50:21 -0300

    removendo o redis

--- docker/custom/entrypoint-custom.sh
+++ docker/custom/entrypoint-custom.sh
@@ -4,27 +4,7 @@
 echo "INICIANDO NGINX"
 nginx
 
-######### REDIS CACHE/SESSION #########
-
-REDIS_SESSION_DIR=${REDIS_SESSION_DIR:-/var/www/var/sessions}
-
-[ ! -e $REDIS_SESSION_DIR ] && mkdir $REDIS_SESSION_DIR || chmod o+rx $REDIS_SESSION_DIR -R
-[ ! -e /etc/redis ] && mkdir /etc/redis || true
-
-REDIS_CACHE_PORT=${REDIS_CACHE_PORT:-6379}
-echo "INICIANDO REDIS-CACHE *:$REDIS_CACHE_PORT"
-echo -e "daemonize yes\nprotected-mode no\nport $REDIS_CACHE_PORT\nmaxmemory 1256Mb\nmaxmemory-policy allkeys-lru\nlogfile /tmp/redis-cache.log\npidfile /var/run/redis-cache.pid" > /etc/redis/redis-cache.conf
-redis-server /etc/redis/redis-cache.conf
-tail -F /tmp/redis-cache.log &
-
-REDIS_SESSION_PORT=${REDIS_SESSION_PORT:-6380}
-echo "INICIANDO REDIS-SESSION *:$REDIS_SESSION_PORT - REDIS_SESSION_DIR:$REDIS_SESSION_DIR"
-echo -e "daemonize yes\nprotected-mode no\nport $REDIS_SESSION_PORT\nmaxmemory 384Mb\nmaxmemory-policy allkeys-lru\ndir $REDIS_SESSION_DIR\nlogfile /tmp/redis-session.log\npidfile /var/run/redis-session.pid" > /etc/redis/redis-session.conf
-redis-server /etc/redis/redis-session.conf
-tail -F /tmp/redis-session.log &
-
 ######### SMTP ##########
-
 RECIPIENT_RESTRICTIONS=static:OK
 
 export SMTP_LOGIN SMTP_PASSWORD RECIPIENT_RESTRICTIONS
